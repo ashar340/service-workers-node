11200916f11506ff6b3d437103146d703180dfbe
Ananya <ananyas237@gmail.com>
2020-08-01 18:39:35 -0400

    updated lifecycle of the sw

--- service-worker-exercise/web/js/sw.js
+++ service-worker-exercise/web/js/sw.js
@@ -1,2 +1,26 @@
 "use strict";
 
+const version = 1;
+
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+
+main().catch(console.error);
+
+async function main() {
+    console.log(`Service worker (${version}) is starting....`);
+}
+
+async function onInstall(evt) {
+    console.log(`Service worker (${version}) is installed`);
+    self.skipWaiting();
+}
+
+function onActivate(evt) {
+    evt.waitUntil(handleActivation());
+}
+
+async function handleActivation() {
+    await clients.claim();
+    console.log(`Service worker (${version}) is being activated....`);
+}
